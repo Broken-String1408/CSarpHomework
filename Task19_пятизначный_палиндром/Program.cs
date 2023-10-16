cc521b2060424eff95102047c542f429d7758388
Artem <kanashko.artemka@yandex.ru>
2023-10-16 03:07:51 +0300

    всем заданиям даны имена + задачи из 3го семинара

---
+++ Task19_пятизначный_палиндром/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+
+Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+
+Для остальных чисел вернуть True или False.
+*/
+System.Console.WriteLine("Enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int revN = 0;
+for (int i = N; i > 0; i /= 10)
+{
+revN = revN * 10 + i % 10;
+//System.Console.WriteLine(revN);
+}
+if (N == revN)
+System.Console.WriteLine("Yes");
+else System.Console.WriteLine("No");
---
+++ Task21_3d_пространство/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Внутри класса Answer напишите метод DistanceBetweenPoints, 
+который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел,
+и возвращает расстояние между ними в 3D пространстве.
+*/
+
+Console.Clear();
+
+System.Console.Write("Задай координату точки A x:");
+
+int userAX = int.Parse(Console.ReadLine());
+
+System.Console.Write("Задай координату точки A y:");
+
+int userAY = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Задай координату точки A z: ");
+
+int userAZ = int.Parse(Console.ReadLine());
+
+
+
+System.Console.Write("Задай координату точки B x:");
+
+int userBX = int.Parse(Console.ReadLine());
+
+System.Console.Write("Задай координату точки B y:");
+
+int userBY = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Задай координату точки B z: ");
+
+int userBZ = int.Parse(Console.ReadLine());
+
+
+
+//int sqrX = (userBX - userAX) * (userBX - userAX);
+
+double sqrX = Math.Pow(userBX - userAX, 2); //Math.Pow(а, 2) функция возводит в степень (переменная, в какую степень надо возвести)
+
+double sqrY = Math.Pow(userBY - userAY, 2);
+
+double sqrZ = Math.Pow(userBZ - userAZ, 2);
+
+double sqrt = Math.Sqrt(sqrX + sqrY + sqrZ); // Math.Sqrt(sqrX + sqrY) выводит квадратный корень из переменной в скобочках
+
+System.Console.WriteLine(Math.Round(sqrt, 2)); // Math.Round(sqrt, 2) округляет десятичное число (переменная, сколько знаков после запятой оставить)
+
+
+// double squareX = Math.Pow(pointB[0] - pointA[0], 2);
+// double squareY = Math.Pow(pointB[1] - pointA[1], 2);
+// double squareZ = Math.Pow(pointB[2] - pointA[2], 2);
+// double result = Math.Sqrt(squareX + squareY + squareZ);
+// return result;
